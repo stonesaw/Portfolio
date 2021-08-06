f088ec7e353f530e26e5bd0f86a91d130efee694
stonesaw <ishichan1260@gmail.com>
2021-08-06 22:46:34 +0900

    Add: About component

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,19 +1,63 @@
 <template>
-  <v-container
-    id="About"
-    class="text--center"
-  >
-  <h1>About</h1>
+  <v-container id="About">
+    <v-row class="text-center">
+      <v-col cols="12">
+        <v-img
+          :aspect-ratio="1/1"
+          :src="require('../assets/icon-high.png')"
+          class="rounded-circle"
+          width="500px"
+          style="display: block; margin: auto;"
+        />
+      </v-col>
+
+      <v-col class="mb-3">
+        <h1 class="display-2 font-weight-bold mb-3">
+          Sou Ishihara
+        </h1>
+
+        <p class="subheading font-weight-regular">
+          学生・ゲーム開発が好き
+          <br>たまに、ドット絵を描きます
+        </p>
+      </v-col>
+
+      <!-- outline -->
+      <v-col
+        cols="12"
+        v-for="outline in outlines"
+        :key="outline.title"
+        class=""
+      >
+        <h2 class="headline font-weight-bold mb-3">
+          {{ outline.title }}
+        </h2>
+        <v-row justify="center">
+          <p class="subheading font-weight-regular">
+            <label
+              v-for="item in outline.items"
+              :key="item"
+              class="mx-3"
+            >
+              {{ item }}
+            </label>
+          </p>
+        </v-row>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
-export default {
-  name: 'About',
-  data() {
-    return {
-      //
-    }
-  },
-}
+  export default {
+    name: 'HelloWorld',
+
+    data: () => ({
+      outlines: [
+        { title: 'School', items: ['松江工業高等専門学校'] },
+        { title: 'Editor', items: ['vscode'] },
+        { title: 'Listen', items: ['millenium parade', '藤井風', 'lofi hip hop'] },
+      ]
+    }),
+  }
 </script>
