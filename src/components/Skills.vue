bbf79e772dd975d99feaed1344dd6a35e12e4b83
stonesaw <ishichan1260@gmail.com>
2021-08-31 02:06:27 +0900

    Add: coloring of skill icons when hovered

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -11,15 +11,36 @@
           <v-row>
             <v-col
               cols="12" sm="6" md="6" lg="4" xl="4"
-              v-for="langage in languages"
-              :key="langage.name"
+              v-for="language in languages"
+              :key="language.name"
             >
               <!-- このコンテナで1つの言語 -->
               <v-container>
                 <v-row>
                   <v-col cos="5">
-                    <v-icon size="64">{{langage.icon}}</v-icon>
-                    <p class="text-overline" style="margin: 0;">{{ langage.name }}</p>
+                    <v-hover v-slot="{ hover }">
+                    <!-- <v-hover> -->
+                      <div v-if="hover">
+                        <v-icon
+                          :color="language.color"
+                          size="64"
+                        >{{language.icon}}</v-icon>
+                        <p 
+                          class="text-overline"
+                          :class="language.color_text"
+                          style="margin: 0;"
+                        >{{ language.name }}</p>
+                      </div>
+                      <div v-else>
+                        <v-icon
+                          size="64"
+                        >{{language.icon}}</v-icon>
+                        <p 
+                          class="text-overline "
+                          style="margin: 0;"
+                        >{{ language.name }}</p>
+                      </div>
+                    </v-hover>
                   </v-col
                   >
                   <v-col cols="7">
@@ -27,7 +48,7 @@
                       column
                     >
                       <v-chip
-                        v-for="lib in langage.libs"
+                        v-for="lib in language.libs"
                         :key="lib"
                         outlined
                         :ripple="false"
@@ -58,12 +79,16 @@ export default {
       {
         name: 'Ruby',
         icon: 'mdi-language-ruby',
+        // color usage
+        // ref https://vuetifyjs.com/ja/styles/colors/#section-30de30c630ea30a230eb30ab30e930fc
+        color: 'red accent-1',
         libs: [
           'DXRuby', 'cli', 'RuboCop', 'YARD'
         ]
       },
       { 
         name: 'Python',
+        color: 'blue lighten-2',
         icon: 'mdi-language-python',
         libs: [
           'Pillow', 'Discord.py', 'Pygame'
@@ -72,6 +97,7 @@ export default {
       { 
         name: 'HTML/CSS',
         icon: 'mdi-language-html5',
+        color: 'orange lighten-2',
         libs: [
           'HTML 5', 'CSS 3', 'Bootstrap'
         ]
@@ -79,11 +105,26 @@ export default {
       {
         name: 'JavaScript',
         icon: 'mdi-language-javascript',
+        color: 'amber lighten-2',
         libs: [
           'npm', 'Vue.js', 'Vuetify', 'Anime.js'
         ]
       }
     ]
-  })
+  }),
+
+  mounted() {
+    this.languages.forEach(language => {
+      let ary = language.color.split(' ');
+      if (ary[0]) {
+        language.color_text = `${ary[0]}--text`;
+      } else if (ary[1]) {
+        language.color_text += ` text--${ary[1]}`;
+      } else if (ary.length >= 3) {
+        console.error('wrong languages.color format!');
+      }
+      console.log(language.color_text);
+    })
+  },
 }
 </script>
