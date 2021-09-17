39e493851351485f9d32663b8855eae3fb178a03
stonesaw <ishichan1260@gmail.com>
2021-09-17 17:39:55 +0900

    add: skills card (refactoring)

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -14,53 +14,12 @@
               v-for="language in languages"
               :key="language.name"
             >
-              <!-- このコンテナで1つの言語 -->
-              <v-container>
-                <v-row>
-                  <v-col cos="5">
-                    <v-hover v-slot="{ hover }">
-                    <!-- <v-hover> -->
-                      <div v-if="hover">
-                        <v-icon
-                          :color="language.color"
-                          size="64"
-                        >{{language.icon}}</v-icon>
-                        <p 
-                          class="text-overline"
-                          :class="language.color_text"
-                          style="margin: 0;"
-                        >{{ language.name }}</p>
-                      </div>
-                      <div v-else>
-                        <v-icon
-                          size="64"
-                        >{{language.icon}}</v-icon>
-                        <p 
-                          class="text-overline "
-                          style="margin: 0;"
-                        >{{ language.name }}</p>
-                      </div>
-                    </v-hover>
-                  </v-col
-                  >
-                  <v-col cols="7">
-                    <v-chip-group
-                      column
-                    >
-                      <v-chip
-                        v-for="lib in language.libs"
-                        :key="lib"
-                        outlined
-                        :ripple="false"
-                      >
-                        {{ lib }}
-                      </v-chip>
-                    </v-chip-group>
-                  </v-col>
-                </v-row>
-              </v-container>
-              <!-- このコンテナで1つの言語 -->
-
+              <SkillsCard
+                :name="language.name"
+                :icon="language.icon"
+                :color="language.color"
+                :libs="language.libs"
+              />
             </v-col>
           </v-row>
         </v-container>
@@ -71,8 +30,14 @@
 </template>
 
 <script>
+import SkillsCard from './SkillsCard'
+
 export default {
   name: 'Skills',
+
+  components: {
+    SkillsCard
+  },
 
   data: () => ({
     languages: [
@@ -112,19 +77,5 @@ export default {
       }
     ]
   }),
-
-  mounted() {
-    this.languages.forEach(language => {
-      let ary = language.color.split(' ');
-      if (ary[0]) {
-        language.color_text = `${ary[0]}--text`;
-      } else if (ary[1]) {
-        language.color_text += ` text--${ary[1]}`;
-      } else if (ary.length >= 3) {
-        console.error('wrong languages.color format!');
-      }
-      console.log(language.color_text);
-    })
-  },
 }
 </script>
---
+++ src/components/SkillsCard.vue
@@ -0,0 +1,69 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="5">
+        <v-hover v-slot="{ hover }">
+          <div v-if="hover">
+            <v-icon
+              :color="color"
+              size="64"
+            >{{ icon }}</v-icon>
+            <p 
+              class="text-overline"
+              :class="color_text"
+              style="margin: 0;"
+            >{{ name }}</p>
+          </div>
+          <div v-else>
+            <v-icon
+              size="64"
+            >{{icon}}</v-icon>
+            <p 
+              class="text-overline"
+              style="margin: 0;"
+            >{{ name }}</p>
+          </div>
+        </v-hover>
+      </v-col
+      >
+      <v-col cols="7">
+        <v-chip-group
+          column
+        >
+          <v-chip
+            v-for="lib in libs"
+            :key="lib"
+            outlined
+            :ripple="false"
+          >
+            {{ lib }}
+          </v-chip>
+        </v-chip-group>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'SkillsCard',
+
+  props: {
+    name: String,
+    icon: String,
+    color: String,
+    libs: Array,
+  },
+
+  mounted() {
+    const ary = this.color.split(' ');
+    if (ary[0]) {
+      this.color_text = `${ary[0]}--text`;
+    } else if (ary[1]) {
+      this.color_text += ` text--${ary[1]}`;
+    } else if (ary.length >= 3) {
+      console.error('wrong languages.color format!');
+    }
+  }
+}
+</script>
