fc5c62d328b200128ffe56ecad3536ed56639a40
stonesaw <mail.sou.dev@gmail.com>
2022-08-18 15:14:35 +0900

    update: SkillCard hover action

--- src/components/SkillsCard.vue
+++ src/components/SkillsCard.vue
@@ -1,14 +1,14 @@
 <template>
   <v-container>
-    <v-row>
-      <v-col cols="5">
-        <v-hover v-slot="{ hover }">
+    <v-hover v-slot="{ hover }">
+      <v-row>
+        <v-col cols="5">
           <div v-if="hover">
             <v-icon
               :color="color"
               size="64"
             >{{ icon }}</v-icon>
-            <p 
+            <p
               class="text-overline"
               :class="color_text"
               style="margin: 0;"
@@ -18,29 +18,26 @@
             <v-icon
               size="64"
             >{{icon}}</v-icon>
-            <p 
+            <p
               class="text-overline"
               style="margin: 0;"
             >{{ name }}</p>
           </div>
-        </v-hover>
-      </v-col
-      >
-      <v-col cols="7">
-        <v-chip-group
-          column
-        >
-          <v-chip
-            v-for="lib in libs"
-            :key="lib"
-            outlined
-            :ripple="false"
-          >
-            {{ lib }}
-          </v-chip>
-        </v-chip-group>
-      </v-col>
-    </v-row>
+        </v-col>
+        <v-col cols="7">
+          <v-chip-group column>
+            <v-chip
+              v-for="lib in libs"
+              :key="lib"
+              outlined
+              :ripple="false"
+            >
+              {{ lib }}
+            </v-chip>
+          </v-chip-group>
+        </v-col>
+      </v-row>
+    </v-hover>
   </v-container>
 </template>
 
@@ -55,15 +52,14 @@ export default {
     libs: Array,
   },
 
-  mounted() {
+  created() {
     const ary = this.color.split(' ');
-    if (ary[0]) {
-      this.color_text = `${ary[0]}--text`;
-    } else if (ary[1]) {
-      this.color_text += ` text--${ary[1]}`;
-    } else if (ary.length >= 3) {
+    if (ary.length >= 3) {
       console.error('wrong languages.color format!');
+      return;
     }
+    if (ary[0]) { this.color_text = `${ary[0]}--text`; }
+    if (ary[1]) { this.color_text += ` text--${ary[1]}`; }
   }
 }
 </script>
